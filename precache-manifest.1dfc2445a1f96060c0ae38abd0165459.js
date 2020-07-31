733f19aaf515717b902aa125fc4b401a79595b0b
maheshnv99 <mahesh.nandini66@gmail.com>
2020-07-31 23:26:51 +0530

    Updates

---
+++ precache-manifest.1dfc2445a1f96060c0ae38abd0165459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fc6cfdd7e9e51d71918a80a7669bb6",
+    "url": "/miscellaneous2/index.html"
+  },
+  {
+    "revision": "ff54329411b74447222f",
+    "url": "/miscellaneous2/static/js/2.421d2280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miscellaneous2/static/js/2.421d2280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf664c4290a5243b0fa",
+    "url": "/miscellaneous2/static/js/main.b833b028.chunk.js"
+  },
+  {
+    "revision": "8e0af1cc0d9724c3650a",
+    "url": "/miscellaneous2/static/js/runtime-main.821faca4.js"
+  }
+]);
